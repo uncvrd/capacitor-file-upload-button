7351a41ec254162c68c3a315c9f10f6ace296dc9
Jordan Lewallen <support@uncvrd.co>
2021-04-21 01:13:11 -0700

    init commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import React, { useRef, useState } from 'react';
 
 const Home: React.FC = () => {
+
+  const pageRef = useRef<HTMLElement | undefined>();
+
+  const [showModal, setShowModal] = useState(false);
+
+  const onPhotoSelect = async () => {
+    try {
+      const photo = await Camera.getPhoto({
+        allowEditing: true,
+        quality: 100,
+        resultType: CameraResultType.DataUrl,
+        webUseInput: true,
+        source: CameraSource.Prompt,
+      });
+      if (photo && photo.dataUrl) {
+      }
+    } catch (error) { }
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <>
+      <IonPage ref={pageRef}>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Blank</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer />
+          <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
+        </IonContent>
+      </IonPage>
+      <IonModal isOpen={showModal} presentingElement={pageRef.current}>
+        <div>Modal Content</div>
+        <IonButton onClick={onPhotoSelect}>Change Photo</IonButton>
+      </IonModal>
+    </>
   );
 };
 
